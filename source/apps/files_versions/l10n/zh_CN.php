1ed2911c7624944b5b362f340e0fcfb022bdc015
jian.hou <jian.hou@cs2c.com.cn>
2016-01-06 19:59:48 +0800

    change translation for file version

--- source/apps/files_versions/l10n/zh_CN.php
+++ source/apps/files_versions/l10n/zh_CN.php
@@ -2,6 +2,9 @@
 $TRANSLATIONS = array(
 "Could not revert: %s" => "无法恢复: %s",
 "Versions" => "版本",
+"Failed to revert {file} to revision {timestamp}." => "无法还原文件 {file} 至版本 {timestamp}",
+"More versions..." => "更多版本...",
+"No other versions available" => "没有其它版本了",
 "Restore" => "恢复"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
